f3839f8b6c425c165ca7f1bd74602495196a7158
cuongSkedulo <cuong.nguyen@skedulo.com>
2021-10-05 09:47:22 +0700

    Done with tutorial 1

--- excercise4/src/main/java/com/example/excercise4/MainActivity.java
+++ excercise4/src/main/java/com/example/excercise4/MainActivity.java
@@ -13,11 +13,17 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    int result = 0;
+    int currentNumber = 0;
+    String currentOperation = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView display2 = (TextView) findViewById(R.id.display2);
+        display2.setText(Integer.toString(result));
 
         ArrayList<View> allButtons =  findViewById(R.id.parent).getTouchables();
         for (View button: allButtons) {
@@ -33,7 +39,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(String inputButton) {
+        if(Utils.isNumberic(inputButton)){
+            handleNumber(Integer.parseInt(inputButton));
+        } else {
+            handleOperation(inputButton);
+        }
+
         TextView display = (TextView) findViewById(R.id.display);
-        display.setText(inputButton);
+        display.setText(Integer.toString(currentNumber));
+        TextView display2 = (TextView) findViewById(R.id.display2);
+        display2.setText(Integer.toString(result)+currentOperation);
+
+    }
+
+    private void handleOperation(String operation) {
+
+        switch (currentOperation) {
+            case "":
+                currentOperation = operation;
+                result = currentNumber;
+                break;
+            case "x":
+                result = result * currentNumber;
+                break;
+            case "/":
+                result = result / currentNumber;
+                break;
+            case "+":
+                result = result + currentNumber;
+                break;
+            case "-":
+                result = result - currentNumber;
+                break;
+        }
+        currentNumber = 0;
+        currentOperation = operation;
+        if(operation.contains("clear")){
+            result = 0;
+            currentOperation = "";
+        }
+        if(operation.contains("=")){
+            currentOperation = "";
+        }
+    }
+
+    private void handleNumber(int number){
+        if(currentNumber == 0){
+            currentNumber = number;
+        } else {
+            currentNumber = currentNumber*10 + number;
+        }
+
     }
 }
---
+++ excercise4/src/main/java/com/example/excercise4/Utils.java
@@ -0,0 +1,7 @@
+package com.example.excercise4;
+
+public class Utils {
+    public static boolean isNumberic(String input){
+        return input.matches("-?\\d+(\\.\\d+)?");
+    }
+}
